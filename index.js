b0d26f3011fbdb0e266e4cb0af77c49f6d3e2e20
Phillipe Tibúrcio <pphillip13@hotmail.com>
2021-05-09 21:42:31 -0300

    adicionado ejs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
-
+const ejs = require('ejs')
 const app = new express()
 
+app.set('view engine','ejs')
 app.use(express.static('public'))
 app.listen(4000, () => {
     console.log('App listening on port 4000')
@@ -13,13 +14,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
